beaf04283edc40585fb28169435cb2ec702083c4
Mqondisi Zulu <mqondisi.zulu@enerserv.co.za>
2026-01-29 14:04:45 +0200

    Use simple MediatR assembly registration

--- RealRelianceBanking.Application/DependencyInjection.cs
+++ RealRelianceBanking.Application/DependencyInjection.cs
@@ -10,10 +10,7 @@ namespace RealRelianceBanking.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
-            services.AddMediatR(config =>
-            {
-                config.RegisterServicesFromAssemblyContaining<DependencyInjection>();
-            });
+            services.AddMediatR(typeof(DependencyInjection).Assembly);
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             return services;
         }
